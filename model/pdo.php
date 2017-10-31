6440e08fb9b0680af9d23c3671de5f8982ed02f6
urbaniak <urbaniaknicolas62@gmail.com>
2017-10-31 15:29:55 +0100

    delete pass database

--- model/pdo.php
+++ model/pdo.php
@@ -2,7 +2,7 @@
 
 function connection(){
   try {
-    $bdd= new PDO('mysql:host=localhost;dbname=evalBlanche;metacharset=utf8','root','chniak24dec78-'
+    $bdd= new PDO('mysql:host=localhost;dbname=evalBlanche;metacharset=utf8','root',''
     , array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 }
  catch (Exception $e)
